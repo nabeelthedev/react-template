f90e375f19228ceb39e4867a53ae8548929afc45
Nabeel Rana <nabeelthedev@gmail.com>
2019-05-26 22:16:22 -0700

    added react-router-dom functionality

--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import SignIn from './pages/SignIn'
+import Home from './pages/Home'
 
 class Main extends Component {
-  render(){
-    return (<div>hello</div>)
+  render() {
+    return (
+      <div>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/signin">Sign In</Link>
+              </li>
+            </ul>
+
+            <hr />
+
+            <Route exact path="/" component={Home} />
+            <Route path="/signin" component={SignIn} />
+          </div>
+        </Router>
+      </div>
+    )
   }
 }
 
-ReactDOM.render(<Main/>, document.getElementById('root'))
+ReactDOM.render(<Main />, document.getElementById('root'))
