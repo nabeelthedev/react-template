a33b195703ae807a4a13f032d511773cd14d2f9e
Nabeel Rana <nabeelthedev@gmail.com>
2019-06-21 17:45:24 -0700

    added private route and authentication functionality

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import SignIn from './pages/SignIn'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter,
+  Switch
+} from 'react-router-dom'
 import Home from './pages/Home'
 
 class Main extends Component {
@@ -10,21 +16,102 @@ class Main extends Component {
       <div>
         <Router>
           <div>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/signin">Sign In</Link>
-              </li>
-            </ul>
-
+            <AuthButton />
             <hr />
-
-            <Route exact path="/" component={Home} />
-            <Route path="/signin" component={SignIn} />
+            <Switch>
+              <PrivateRoute exact path="/" component={Home} />
+              <Route path="/signin" component={SignIn} />
+              <Redirect to={{ pathname: '/' }} />
+            </Switch>
           </div>
         </Router>
+      </div>
+    )
+  }
+}
+
+const sessionIdExists = () => {
+  if (localStorage.getItem('sessionId')) {
+    return true
+  } else {
+    return false
+  }
+}
+
+const authHandler = {
+  isAuthenticated: sessionIdExists,
+  authenticate(func) {
+    localStorage.setItem('sessionId', '123')
+    func()
+  },
+  signout(func) {
+    localStorage.clear()
+    func()
+  }
+}
+
+const AuthButton = withRouter(({ history }) =>
+  authHandler.isAuthenticated() ? (
+    <p>
+      <Link to="/">Home</Link>
+      <button
+        onClick={() => {
+          authHandler.signout(() => history.push('/'))
+        }}
+      >
+        Sign out
+      </button>
+    </p>
+  ) : (
+    <p>You are not logged in.</p>
+  )
+)
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authHandler.isAuthenticated() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/signin',
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  )
+}
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { redirectToReferrer: false }
+    this.login = this.login.bind(this)
+  }
+
+  login() {
+    authHandler.authenticate(() => {
+      this.setState({ redirectToReferrer: true })
+    })
+  }
+
+  render() {
+    let { from } = this.props.location.state || { from: { pathname: '/' } }
+    let { redirectToReferrer } = this.state
+
+    if (redirectToReferrer) return <Redirect to={from} />
+    if (authHandler.isAuthenticated())
+      return <Redirect to={{ pathname: '/' }} />
+
+    return (
+      <div>
+        <p>You must log in to view the page at {from.pathname}</p>
+        <button onClick={this.login}>Log in</button>
       </div>
     )
   }
