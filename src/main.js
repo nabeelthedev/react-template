b5368a8017d9fb37b654d29112b0ecff4c90136e
Nabeel Rana <nabeelthedev@gmail.com>
2019-06-22 00:42:34 -0700

    fixed authHandler and redirectToReferrer, added loading to SignIn

--- src/main.js
+++ src/main.js
@@ -30,23 +30,21 @@ class Main extends Component {
   }
 }
 
-const sessionIdExists = () => {
-  if (localStorage.getItem('sessionId')) {
-    return true
-  } else {
-    return false
-  }
-}
-
 const authHandler = {
-  isAuthenticated: sessionIdExists,
-  authenticate(func) {
-    localStorage.setItem('sessionId', '123')
-    func()
+  isAuthenticated() {
+    if (localStorage.getItem('sessionId') != null) {
+      return true
+    } else {
+      return false
+    }
   },
-  signout(func) {
+  authenticate(input) {
+    localStorage.setItem('sessionId', 'data')
+    input.redirectToReferrer()
+  },
+  signout(pushToSignInPage) {
     localStorage.clear()
-    func()
+    pushToSignInPage()
   }
 }
 
@@ -56,14 +54,14 @@ const AuthButton = withRouter(({ history }) =>
       <Link to="/">Home</Link>
       <button
         onClick={() => {
-          authHandler.signout(() => history.push('/'))
+          authHandler.signout(() => history.push('/signin'))
         }}
       >
         Sign out
       </button>
     </p>
   ) : (
-    <p>You are not logged in.</p>
+    <p>Sign In</p>
   )
 )
 
@@ -90,31 +88,75 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
 class SignIn extends Component {
   constructor(props) {
     super(props)
-    this.state = { redirectToReferrer: false }
-    this.login = this.login.bind(this)
-  }
-
-  login() {
-    authHandler.authenticate(() => {
-      this.setState({ redirectToReferrer: true })
-    })
+    this.state = { redirectToReferrer: false, loading: false }
   }
 
   render() {
     let { from } = this.props.location.state || { from: { pathname: '/' } }
+    from.pathname != '/'
+      ? sessionStorage.setItem('from', JSON.stringify(from))
+      : false
+    let storedFrom = JSON.parse(sessionStorage.getItem('from'))
+    storedFrom ? (from = storedFrom) : false
     let { redirectToReferrer } = this.state
 
     if (redirectToReferrer) return <Redirect to={from} />
     if (authHandler.isAuthenticated())
       return <Redirect to={{ pathname: '/' }} />
 
-    return (
-      <div>
-        <p>You must log in to view the page at {from.pathname}</p>
-        <button onClick={this.login}>Log in</button>
-      </div>
-    )
+    if (this.state.loading) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <div>
+          <button
+            onClick={() => {
+              localStorage.setItem('sessionId', 'data')
+              this.setState({ redirectToReferrer: true })
+            }}
+          >
+            Log in
+          </button>
+        </div>
+      )
+    }
   }
+
+  componentDidMount() {
+    const parsedUrl = new URL(window.location.href)
+    const code = parsedUrl.searchParams.get('code')
+    if (code) {
+      this.setState({ loading: true })
+      authHandler.authenticate({
+        code: code,
+        redirectToReferrer: () => {
+          this.setState({ redirectToReferrer: true, loading: false })
+        }
+      })
+    }
+  }
+}
+
+const postData = async (url = '', data = {}) => {
+  // Default options are marked with *
+  let fetchResponse = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    // mode: 'no-cors', // no-cors, cors, *same-origin
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json().then(data => {
+          return data
+        })
+      }
+    })
+    .catch(err => {})
+  return fetchResponse
 }
 
 ReactDOM.render(<Main />, document.getElementById('root'))
